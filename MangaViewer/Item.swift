abac36254f826933a2e0485e9a3a4b90fcd81961
Sahil Arvind Ramteke <45787960+sahilramteke@users.noreply.github.com>
2025-07-07 14:40:17 +0530

    Initial Commit

---
+++ MangaViewer/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MangaViewer
+//
+//  Created by Sahil Ramteke on 07/07/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MangaViewerTests/MangaViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MangaViewerTests.swift
+//  MangaViewerTests
+//
+//  Created by Sahil Ramteke on 07/07/25.
+//
+
+import Testing
+@testable import MangaViewer
+
+struct MangaViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
